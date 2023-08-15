8805ad41d1dc3eeb7e0e524c9a28a6814e17b1f7
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-16 01:51:05 +0600

    resume added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,26 @@ import { styles } from '../../styles'
 import { navLinks } from '../../constants'
 import { logo, menu, close } from '../../assets'
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 const Navbar = () => {
     const [active, setActive] = useState("");
     const [toggle, setToggle] = useState(false);
     const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollTop = window.scrollY;
+          if (scrollTop > 100) {
+            setScrolled(true);
+          } else {
+            setScrolled(false);
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => window.removeEventListener("scroll", handleScroll);
+      }, []);
 
     return (
         <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 ${scrolled ? "bg-primary" : "bg-transparent"
@@ -23,7 +38,7 @@ const Navbar = () => {
                         Rasik&nbsp;<span className='sm:block hidden'>Abdullah</span>
                     </p>
                 </Link>
-                <ul className='list-none hidden sm:flex flex-row gap-10'>
+                <ul className='list-none hidden sm:flex flex-row items-center gap-10'>
                     {
                         navLinks.map((link) => (
                             <li key={link.id}
@@ -35,6 +50,7 @@ const Navbar = () => {
                             </li>
                         ))
                     }
+                    <a href="https://drive.google.com/u/0/uc?id=1QyyNUnaj3dhWjjlnxV_awH4kMLJR4fBp&export=download" target='_blank' rel='noreferrer' className='text-secondary text-[18px] font-medium cursor-pointer'>Resume</a>
                 </ul>
 
                 <div className='sm:hidden flex flex-1 justify-end items-center'>
