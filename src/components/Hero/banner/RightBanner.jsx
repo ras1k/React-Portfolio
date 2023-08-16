9cc72592e096864e858c7587dccec1b08e75e0fe
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-16 17:40:54 +0600

    fixed image

--- src/components/Hero/banner/RightBanner.jsx
+++ src/components/Hero/banner/RightBanner.jsx
@@ -8,7 +8,7 @@ const RightBanner = () => {
         src={bannerImg}
         alt="bannerImg"
       />
-      <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#915eff3b] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      <div className="absolute bottom-0 w-[400px] h-[350px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#915eff3b] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
 }
