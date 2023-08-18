6e787fa57dfde128429040a1b7807f96a628c79f
Rasik Abdullah <rasikabdullah@gmail.com>
2023-08-18 13:13:01 +0600

    changed mobile navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { navLinks } from '../../constants'
 import { logo, menu, close } from '../../assets'
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
     const [active, setActive] = useState("");
@@ -57,9 +58,9 @@ const Navbar = () => {
                 <div className='sm:hidden flex flex-1 justify-end items-center'>
                     <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
 
-                    <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+                    <div className={`${!toggle ? 'hidden' : 'flex'}`}>
 
-                        <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                        {/* <ul className='list-none flex justify-end items-start flex-col gap-4'>
                             {
                                 navLinks.map((link) => (
                                     <li key={link.id}
@@ -74,8 +75,65 @@ const Navbar = () => {
                                 ))
                             }
                             <a href="https://drive.google.com/file/d/1QyyNUnaj3dhWjjlnxV_awH4kMLJR4fBp/view?usp=sharing" target='_blank' rel='noreferrer' className='text-secondary text-[18px] font-medium cursor-pointer'>Resume</a>
+                        </ul> */}
 
-                        </ul>
+
+                        <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
+                            <div className="flex flex-col gap-8 py-2 relative">
+                                <div>
+                                    <div className='flex items-center gap-4'>
+                                        <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+                                        <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+                                            Rasik&nbsp;<span className='sm:block hidden'>Abdullah</span>
+                                        </p>
+                                    </div>
+                                    <p className="text-sm text-gray-400 mt-2">
+                                        I am a front-end web developer specializing in React.js. With a strong foundation in HTML, CSS, and JavaScript.
+                                    </p>
+                                </div>
+                                <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                                    {
+                                        navLinks.map((link) => (
+                                            <li key={link.id}
+                                                className={`${active === link.title ? 'text-white' : 'text-secondary'} font-poppins font-medium cursor-pointer text-[16px]`}
+                                                onClick={() => {
+                                                    setToggle(!toggle);
+                                                    setActive(link.title);
+                                                }}
+                                            >
+                                                <a href={`#${link.id}`}>{link.title}</a>
+                                            </li>
+                                        ))
+                                    }
+                                    <a href="https://drive.google.com/file/d/1QyyNUnaj3dhWjjlnxV_awH4kMLJR4fBp/view?usp=sharing" target='_blank' rel='noreferrer' className='text-secondary text-[18px] font-medium cursor-pointer'>Resume</a>
+                                </ul>
+                                <div className="flex flex-col gap-4">
+                                    <h2 className="text-base uppercase font-titleFont mb-4">
+                                        Find me in
+                                    </h2>
+                                    <div className="flex gap-4">
+                                        <span className="w-16 h-16 bg-black bg-opacity-25 text-gray-200 text-xl inline-flex items-center justify-center rounded-md shadow-shadowOne hover:bg-opacity-40 hover:-translate-y-1 transition-all hover:text-designColor cursor-pointer duration-300;">
+                                            <a href="https://www.facebook.com/ras1k/" target="_blank" rel="noreferrer"><FaFacebookF /></a>
+                                        </span>
+                                        <span className="w-16 h-16 bg-black bg-opacity-25 text-gray-200 text-xl inline-flex items-center justify-center rounded-md shadow-shadowOne hover:bg-opacity-40 hover:-translate-y-1 transition-all hover:text-designColor cursor-pointer duration-300;">
+                                            <a href="https://twitter.com/ras1k_" target="_blank" rel="noreferrer"><FaTwitter /></a>
+                                        </span>
+                                        <span className="w-16 h-16 bg-black bg-opacity-25 text-gray-200 text-xl inline-flex items-center justify-center rounded-md shadow-shadowOne hover:bg-opacity-40 hover:-translate-y-1 transition-all hover:text-designColor cursor-pointer duration-300;">
+                                            <a href="https://github.com/ras1k" target="_blank" rel="noreferrer"><FaGithub /></a>
+                                        </span>
+                                        <span className="w-16 h-16 bg-black bg-opacity-25 text-gray-200 text-xl inline-flex items-center justify-center rounded-md shadow-shadowOne hover:bg-opacity-40 hover:-translate-y-1 transition-all hover:text-designColor cursor-pointer duration-300;">
+                                            <a href="https://www.linkedin.com/in/ras1k/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+                                        </span>
+                                    </div>
+                                </div>
+                                {/* <span
+                                    onClick={() => setShowMenu(false)}
+                                    className="absolute top-4 right-4 text-gray-400 hover:text-designColor duration-300 text-2xl cursor-pointer"
+                                >
+                                    <MdClose />
+                                </span> */}
+                            </div>
+                        </div>
 
                     </div>
                 </div>
